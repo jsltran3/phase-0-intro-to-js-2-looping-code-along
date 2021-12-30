ddcc0e5e8202a65efb403f1c6e83e5813034ebb1
Joseph <jsltran@gmail.com>
2021-12-30 15:01:41 -0800

    commit tree e206d0ccf3061f14a04f02dfc158c60d0d944ed0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, specialEvent) {
+    const newCards = []; 
+    for (let i = 0; i < arr.length; i++) {
+    let msg = `Thank you, ${arr[i]}, for the wonderful ${specialEvent} gift!`;
+    newCards.push(msg);
+  }
+  return newCards; 
+}
+
+function countDown() {
+    let startingCount = 11; 
+    while (startingCount > 0) {
+        console.log(--startingCount);
+        // --i; 
+    }
+}
